ed32527e3a46c26bf548ff1b32409691601afd33
Saleha Khan <161796561+Ibrahimsabir@users.noreply.github.com>
2024-12-03 19:33:42 -0800

    hover effect

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -6,48 +6,58 @@ import { IoIosBook } from "react-icons/io";
 import { IoLocation } from "react-icons/io5";
 import { FiTarget } from "react-icons/fi";
 
-
-const CardData :{icon:unknown,title:string,description:string,bgColor:string}[]=
+const CardData :{icon:unknown,title:string,description:string,bgColor:string,darkenColor:string}[]=
 [
     {icon:(<FaSearch/>) as unknown as string,
         title: "Search",
         description: "Search for information on any topic",
-        bgColor:"#a0f6aa "
+        bgColor:"#a0f6aa ",
+        darkenColor:"#0d821a"
     },
     {icon:(< FaTag/>) as unknown as string,
         title: "Search",
         description: "Search for information on any topic",
-          bgColor:"#b07be8 "
+          bgColor:"#b07be8 ",
+          darkenColor:""
     },
     {icon:(< FaStar/>) as unknown as string,
         title: "Search",
         description: "Search for information on any topic",
-          bgColor:"#e8e07b "
+          bgColor:"#e8e07b ",
+            darkenColor:""
     },
     {icon:(< IoIosBook />) as unknown as string,
         title: "Search",
         description: "Search for information on any topic",
-          bgColor:"#f6cfc7"
+          bgColor:"#f6cfc7",
+            darkenColor:""
     },
     {icon:(< IoLocation/>) as unknown as string,
         title: "Search",
         description: "Search for information on any topic",
-          bgColor:"#a0ccf6 "
+          bgColor:"#a0ccf6 ",
+            darkenColor:""
         
     },
     {icon:(<FiTarget/>) as unknown as string,
         title: "Search",
         description: "Search for information on any topic",
-          bgColor:"#ebcac4 "
+          bgColor:"#ebcac4 ",
+            darkenColor:""
     },
 
 ]
 
+  
+
 export const Card = () => {
+    
   return (
     <div className='grid sm:grid-cols-1 md:grid-cols-[1fr_1fr_1fr] gap-x-4 gap-y-6 justify-center items-center w-full m-auto '>
+        
         {CardData.map((card, index) => (
-          <div key={index} className={` flex flex-col items-center justify-center w-1/2 h-40 ${card.bgColor} rounded-md shadow-md p-4`}
+            
+          <div key={index} className={` flex flex-col items-center justify-center hover:shadow-lg w-1/2 h-40 rounded-md  p-4`}
           style={{backgroundColor:card.bgColor}}>
             <div className="flex items-center justify-evenly w-full h-full m-auto">
               <div className="items-center justify-center w-8 h-8 text-3xl">
